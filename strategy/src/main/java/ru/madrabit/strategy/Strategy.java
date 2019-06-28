81fc4955232d5c3d630f846d2d6fdcef8c1fdc8e
madrabit <madrabot@gmail.com>
2019-06-28 15:40:51 +0300

    change all project from barbell to strategy

---
+++ strategy/src/main/java/ru/madrabit/strategy/Strategy.java
@@ -0,0 +1,19 @@
+package ru.madrabit.strategy;
+
+/**
+ * Calc for optimal barbell weight.
+ * @author Madrabit
+ */
+
+public class Strategy {
+
+    /**
+     * Main
+     * @param args - args
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ strategy/src/test/java/ru/madrabit/barbell/StrategyTest.java
@@ -0,0 +1,30 @@
+package ru.madrabit.barbell;
+
+import org.junit.Test;
+import ru.madrabit.strategy.Strategy;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author madrabit
+ * @version $ID$
+ * @since  0.1
+ */
+
+public class StrategyTest {
+    /**
+     * Test barbell weight
+     */
+
+    @Test
+    public void whenTakeArrayThenWeight() {
+        Strategy barb = new Strategy();
+//        int result = barb.calc(new int[]{1,2,3,6});
+//        double expected = 12;
+//        assertThat(result, is(expected));
+    }
+
+}
