32de31612c019ad40e62f4f6c20539687d647660
madrabit <madrabot@gmail.com>
2019-07-01 17:54:10 +0300

    random

--- strategy/src/main/java/ru/madrabit/strategy/Strategy.java
+++ strategy/src/main/java/ru/madrabit/strategy/Strategy.java
@@ -1,5 +1,7 @@
 package ru.madrabit.strategy;
 
+import java.util.Random;
+
 /**
  * Calc for optimal strategy weight.
  * @author Madrabit
@@ -12,10 +14,24 @@ class Strategy {
      * @param args - args
      */
 
+    static String[] lightRaces = new String[]{"elf", "human"};
+    static String[] darkRaces = new String[]{"orc", "undead"};
+
     public static void main(String[] args) {
         Squad orcs = new Squad("orc");
         Squad humans = new Squad("human");
         Squad undead = new Squad("undead");
+
+        Random rand = new Random();
+        int n = rand.nextInt(1);
+
+        int random = (int)(Math.round(Math.random()));
+
+        Squad lightSquade = new Squad(lightRaces[random]);
+        Squad darkSquade = new Squad(darkRaces[random]);
+
+        System.out.println(lightSquade);
+        System.out.println(darkSquade);
 
         Wizard wiz = (Wizard) humans.squad.get(0);
         Character fighter = humans.squad.get(6);
